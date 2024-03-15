2f5226a70192bb86fd3daec8c6af566e451a8804
Alina Carpio <agcarpio@espol.edu.ec>
2024-03-15 16:03:47 -0500

    FEAT: Add Artists components

---
+++ frontend/src/components/Artist.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+  url: {
+    type: String,
+    required: true
+  },
+  artistName: {
+    type: String,
+    required: true
+  },
+  listeners: {
+    type: String,
+    required: true
+  },
+})
+</script>
+
+<template>
+  <div class="flex justify-between pt-2 pb-2 pr-4 pl-4 items-center border rounded-xl shadow-lg m-2">
+    <img src="https://via.placeholder.com/100" :src=url class="rounded rounded-full w-16 h-16"/>
+    <h3 class="text-sm">{{ artistName }} Name</h3>
+    <span class="text-xs">{{ listeners }} Listeners</span>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/TopArtists.vue
@@ -0,0 +1,26 @@
+<script setup>
+import Artist from "./Artist.vue";
+
+defineProps({
+  artists: {
+    type: Array,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="flex justify-end">
+    <div class="flex flex-col justify-center w-96">
+      <Artist
+          v-for="artist of artists"
+          :url=artist.url :artistName=artist.artistName :listeners=artist.listeners
+      />
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
